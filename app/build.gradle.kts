5fb2ff67d1986c870650a6c0733aa477b9f3bfd6
Arsenii <you@example.com>
2026-09-13 23:19:58 +0200

    update: 2026-09-13 23:19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -226,9 +226,9 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 
